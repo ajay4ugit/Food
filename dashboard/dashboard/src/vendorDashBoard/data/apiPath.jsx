f1f1238476b822af9ce62b27da16fed594397931
ajayreddy <ajay4ugit@gmail.com>
2024-08-02 16:28:17 +0530

    final commit

--- dashboard/dashboard/src/vendorDashBoard/data/apiPath.jsx
+++ dashboard/dashboard/src/vendorDashBoard/data/apiPath.jsx
@@ -1,2 +1,3 @@
-export const API_URL= "http://localhost:5000";
+//export const API_URL= "http://localhost:5000";
 
+export const API_URL= "https://vercel.com/ajay4ugits-projects/food/D7WWQy9tN2PRCdmuF6sqruiCWj7Q";
